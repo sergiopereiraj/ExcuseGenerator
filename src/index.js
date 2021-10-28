91a49aa82bb44c2159787661176bae74577b97c8
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-10-28 13:31:37 +0000

    guardado

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "../style/index.scss"
+
+window.onload = () => {
+document.querySelector('#btn').addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse(); 
+});
+    console.log('Hello Rigo from the console! ');
+};
+let generateExcuse = () => {
+
+    let pronoun = ['A', 'The'];
+    let subjet = ['jogger','racoon','dog','driver','comedian','pincone'];
+    let action = ['took my','threw my','yelled at my','stole my','bit my'];
+    let possetion = ['homework','toe','car','shoe'];
+    let where = ['on the street','in my house','in my driveway'];
+
+let proIndx = Math.floor(Math.random () * pronoun.length);
+let subjIndx = Math.floor(Math.random () * subjet.length);
+let actionIndex = Math.floor(Math.random () * action.length);
+let possetionIndex = Math.floor(Math.random () * possetion.length);
+let whereIndex = Math.floor(Math.random () * where.length);
+
+  return pronoun[proIndx] + ' ' + subjet[subjIndx] + ' ' + action[actionIndex] + ' '+ possetion[possetionIndex] + ' '+ where[whereIndex];
+};
